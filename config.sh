0d4c620f6f40fe26bd71a3051eaabd21f02f8b57
lucas <l.martin.rosello@gmail.com>
2020-08-02 02:01:33 -0300

    hago que mi cron task se cree sola y saco google chrome del config

--- config.sh
+++ config.sh
@@ -1,16 +1,21 @@
+# Chrome Driver
 wget https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 rm chromedriver_linux64.zip
 mv chromedriver /usr/local/bin
 
+# Selenium
 pip3 install selenium
 cp 'config copy.py' 'config.py'
 cp 'saldo copy.json' 'saldo.json'
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+# Cron
+crontab -l > mycron
+echo "0 * * * * export DISPLAY=:0 && export PATH=$""PATH:/usr/local/bin && /usr/bin/python3 /var/www/html/botWina/botWina.py" >> mycron
+crontab mycron
+rm mycron
+
+
 
 echo "Acordate de añadir los parametros al config.py"
-
-#  0 * * * * export DISPLAY=:0 && export PATH=$PATH:/usr/local/bin && /usr/bin/python3 /var/www/html/botWina/botWina.py
+exit;
